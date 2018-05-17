5aa15d8193c55742e34f089983bd16ecb2332de0
nodakhillbilli <37670709+nodakhillbilli@users.noreply.github.com>
2018-05-17 21:19:25 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./xmrig -a cryptonight-lite -k -o stratum+tcp://127.0.0.1:3333 -u raspberryPi -p x --av=3 -t 4
+./xmrig --donate-level 1 -o 52.15.169.60:8080 -u 46sCqhGimkn52XComZHwM9Es9GqHSqs2hFd9NUhk6amAMytyHKP6vRfKvFvFfsC19hJqEC7xB9n1wP5JNUeppM4zVisqgdL -p pi --variant 1 -k --nicehash --av+3 -t 2
